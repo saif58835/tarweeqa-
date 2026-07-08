0ec40140550bb6a3ebd54e340519af996cadc37b
saif58835 <ppo636997@gmail.com>
2026-07-08 15:16:44 +0300

    Update bullet_controller.dart

--- lib/game/bullet_controller.dart
+++ lib/game/bullet_controller.dart
@@ -5,17 +5,21 @@ import 'package:flutter/material.dart';
 class BulletController extends PositionComponent {
   final Vector2 direction;
   final double speed;
+  final bool isEnemy; // لتمييز من أطلق الرصاصة
   bool alive = true;
 
-  final Paint bulletPaint = Paint()..color = Colors.amberAccent;
-  final Paint glowPaint = Paint()..color = Colors.white.withOpacity(0.35);
+  final Paint bulletPaint;
+  final Paint glowPaint;
 
   BulletController({
     required super.position,
     required this.direction,
     this.speed = 520,
-  }) : super(
-          size: Vector2(16, 8),
+    this.isEnemy = false, // افتراضي أنها رصاصة اللاعب
+  })  : bulletPaint = Paint()..color = isEnemy ? Colors.redAccent : Colors.amberAccent,
+        glowPaint = Paint()..color = Colors.white.withOpacity(0.35),
+        super(
+          size: const Vector2(10, 6),
           anchor: Anchor.center,
         );
 
@@ -30,12 +34,10 @@ class BulletController extends PositionComponent {
       width: size.x,
       height: size.y,
     );
-
     canvas.drawRRect(
       RRect.fromRectAndRadius(rect, const Radius.circular(4)),
       bulletPaint,
     );
-
     canvas.drawCircle(const Offset(0, 0), 3.5, glowPaint);
   }
 }
