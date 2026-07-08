967098aea1c6bb6894a56bc3200f9be5dde5e71d
saif58835 <ppo636997@gmail.com>
2026-07-08 15:04:24 +0300

    Update game_screen.dart

--- lib/game/game_screen.dart
+++ lib/game/game_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'battle_game.dart';
+import '../network/network_service.dart';
 
 class GameScreen extends StatelessWidget {
-  const GameScreen({super.key});
+  final NetworkService networkService;
+
+  const GameScreen({super.key, required this.networkService});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GameWidget(
-        game: BattleGame(),
+        game: BattleGame(networkService: networkService),
       ),
     );
   }
